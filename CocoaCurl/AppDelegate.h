4052f7a019dfc3825dee4258e80915728e605211
Michael Jones <mikej253@me.com>
2015-05-09 08:38:24 +1000

    Initial Commit

---
+++ CocoaCurl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CocoaCurl
+//
+//  Created by Michael Jones on 9/05/2015.
+//  Copyright (c) 2015 Michael Jones. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ CocoaCurl/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CocoaCurl
+//
+//  Created by Michael Jones on 9/05/2015.
+//  Copyright (c) 2015 Michael Jones. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
